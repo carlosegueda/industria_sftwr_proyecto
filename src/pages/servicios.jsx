576ea40fa13d369b8db233797abf7f119e810d21
Carlos Osegueda <carlosegueda200@gmail.com>
2025-07-18 23:29:11 -0600

    Iniciado servicios

--- src/pages/servicios.jsx
+++ src/pages/servicios.jsx
@@ -1,9 +1,23 @@
 import Navbar from "../components/navbar";
+import "../css/servicios.css";
+
 export default function PaginaServicios() {
   return (
     <>
-        <Navbar/>
-      <h1>SERVICIOS</h1>
+      <Navbar />
+      <div className="titulo">
+        <h1>Nuestros Servicios</h1>
+      </div>
+
+      <div className="servicios">
+        <div className="servicios-servicios">
+          <h1>Contamos con una amplia variedad de servicios.</h1>
+          <div className="listaservicios">
+            <div className="s1"><h2>S1</h2></div> <div className="s1"><h2>S2</h2></div>
+            <div className="s1"><h2>S3</h2></div> <div className="s1"><h2>S4</h2></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
