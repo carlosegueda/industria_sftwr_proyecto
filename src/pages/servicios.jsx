4f4eaa5f3abd5931ff00aa4ac5fe4478e97340bb
Carlos Osegueda <carlosegueda200@gmail.com>
2025-07-27 00:28:14 -0600

    Avances en servicios

--- src/pages/servicios.jsx
+++ src/pages/servicios.jsx
@@ -1,7 +1,25 @@
 import Navbar from "../components/navbar";
 import "../css/servicios.css";
+import React, { useState } from "react";
+
+
 
 export default function PaginaServicios() {
+
+  const [contenido, setContenido] = useState("Haz clic en un servicio para ver su información.");
+
+  const infoServicios = {
+    1: "Servicio 1. Este servicio hace lo siguiente bla bla bla",
+    2: "Servicio 2. Este servicio hace lo siguiente bla bla bla",
+    3: "Servicio 3. Este servicio hace lo siguiente bla bla bla",
+    4: "Servicio 4. Este servicio hace lo siguiente bla bla bla",
+  };
+
+  const mostrarInfo = (id) => {
+    setContenido(infoServicios[id]);
+  };
+
+
   return (
     <>
       <Navbar />
@@ -13,9 +31,14 @@ export default function PaginaServicios() {
         <div className="servicios-servicios">
           <h1>Contamos con una amplia variedad de servicios.</h1>
           <div className="listaservicios">
-            <div className="s1"><h2>S1</h2></div> <div className="s1"><h2>S2</h2></div>
-            <div className="s1"><h2>S3</h2></div> <div className="s1"><h2>S4</h2></div>
+            <div className="s1" onClick={() => mostrarInfo(1)}><h2>S1</h2></div> <div className="s1" onClick={() => mostrarInfo(2)}><h2>S2</h2></div>
+            <div className="s1" onClick={() => mostrarInfo(3)}><h2>S3</h2></div> <div className="s1" onClick={() => mostrarInfo(4)}><h2>S4</h2></div>
           </div>
+        </div>
+
+        <div className="servicios-info">
+          <div className="info">{contenido}</div>
+          <div>INFO</div>
         </div>
       </div>
     </>
