2059aed6b7cd41ff4269ebc8b191dfd36d2ed4b3
Carlos Osegueda <carlosegueda200@gmail.com>
2025-08-01 19:47:07 -0600

    Modificacion servicios

--- src/pages/servicios.jsx
+++ src/pages/servicios.jsx
@@ -9,10 +9,11 @@ export default function PaginaServicios() {
   const [contenido, setContenido] = useState("Haz clic en un servicio para ver su información.");
 
   const infoServicios = {
-    1: "Servicio 1. Este servicio hace lo siguiente bla bla bla",
-    2: "Servicio 2. Este servicio hace lo siguiente bla bla bla",
-    3: "Servicio 3. Este servicio hace lo siguiente bla bla bla",
-    4: "Servicio 4. Este servicio hace lo siguiente bla bla bla",
+    1: "Control inteligente de inventario personal de medicamentos.",
+    2: "Geolocalización y mapeo de farmacias cercanas.",
+    3: "Alertas o avisos de consumo y vencimiento.",
+    4: "Historial detallado de consumo para seguimiento médico.",
+    5: "Motor de recomendaciones y publicidad segmentada."
   };
 
   const mostrarInfo = (id) => {
@@ -31,13 +32,14 @@ export default function PaginaServicios() {
         <div className="servicios-servicios">
           <h1>Contamos con una amplia variedad de servicios.</h1>
           <div className="listaservicios">
-            <div className="s1" onClick={() => mostrarInfo(1)}><h2>S1</h2></div> <div className="s1" onClick={() => mostrarInfo(2)}><h2>S2</h2></div>
-            <div className="s1" onClick={() => mostrarInfo(3)}><h2>S3</h2></div> <div className="s1" onClick={() => mostrarInfo(4)}><h2>S4</h2></div>
+            <div className="s1" onClick={() => mostrarInfo(1)}> <img src="s1.png"/></div> <div className="s2" onClick={() => mostrarInfo(2)}><img src="s2.png"/></div>
+            <div className="s3" onClick={() => mostrarInfo(3)}><img src="s3.png"/></div> <div className="s4" onClick={() => mostrarInfo(4)}><img src="s4.png"/></div>
+            <div className="s5" onClick={() => mostrarInfo(5)}><img src="s5.png"/></div>
           </div>
         </div>
 
         <div className="servicios-info">
-          <div className="info">{contenido}</div>
+          <div className="info"> <h3>{contenido}</h3></div>
           <div>INFO</div>
         </div>
       </div>
